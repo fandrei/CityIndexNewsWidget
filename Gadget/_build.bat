e992679996f15ae7682bf7aaff1fe61aaa6f3e7d
fandrei <C:\Users\Andrei\AppData\Roaming\The Bat!>
2011-03-05 11:52:49 +0700

    GUI improvements

--- Gadget/_build.bat
+++ Gadget/_build.bat
@@ -1,1 +1,3 @@
 ..\utils\zipit.exe CityIndexNews.gadget gadget.xml -D ..\CityIndexNewsWidget.Web\ gadget.html Silverlight.js -d ClientBin -D ..\CityIndexNewsWidget.Web\ClientBin CityIndexNewsWidget.xap
+"C:\Program Files (x86)\Windows Sidebar\sidebar.exe"
+start CityIndexNews.gadget
